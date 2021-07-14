d611220b9a9622e7170f5cbea83e1fd6d17add0c
DamianoTrifone <trifone.damiano@gmail.com>
2021-07-14 16:30:44 +0200

    Card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,12 @@
 @foreach ($movies as $movie)
     <h1>{{$movie -> title}}</h1>
 @endforeach
+
+<style>
+    h1{
+        float: left;
+        padding: 25px;
+        border: 1px solid black;
+        margin: 35px;
+    }
+</style>
